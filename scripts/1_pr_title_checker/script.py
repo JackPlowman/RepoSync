3d3be718ddabc46d0a6692d996cd70ea6de3eaeb
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2024-11-16 21:43:36 +0000

    update

--- scripts/1_pr_title_checker/script.py
+++ scripts/1_pr_title_checker/script.py
@@ -1,36 +1,23 @@
 # /// script
 # requires-python = ">=3.13"
-# dependencies = [
-#     "ruamel-yaml==0.18.6",
-# ]
+# dependencies = []
 # ///
-
-import sys
-from ruamel.yaml import YAML
-import os
 
 def update_pr_title_checker(file_path: str) -> bool:
     try:
         # Load the file
-        yaml = YAML(typ='safe', pure=True)
         with open(file_path, 'r') as file:
-            content = yaml.load(file)
-        
+            content = file.read()
         # Transform the content
-        transformed_content = transform_yaml(content)
-        
+        transformed_content = transform_file(content)
         # Save the transformed content back to the file
         with open(file_path, 'w') as file:
-            yaml.dump(transformed_content, file)
-        
-        print(f"File {file_path} updated successfully")
+            file.write(transformed_content)
         return True
-    except Exception as e:
-        print(f"Error updating the file {file_path}: {e}")
+    except Exception:
         return False
 
-def transform_yaml(content: dict) -> dict:
-    # Transform the yaml content
+def transform_file(content: str) -> str:
     content_str = str(content)
     content_str = content_str.replace("check-pr-title", "check-pull-request-title")
     content_str = content_str.replace("Check PR title", "Check Pull Request title")
@@ -38,13 +25,12 @@ def transform_yaml(content: dict) -> dict:
         "\"feat: ,fix: ,bug: ,ci: ,refactor: ,docs: ,build: ,chore(,deps(,chore: ,feat!: ,fix!: ,refactor!: ,test: \"",
         "\"feat: ,fix: ,bug: ,ci: ,refactor: ,docs: ,build: ,chore(,deps(,chore: ,feat!: ,fix!: ,refactor!: ,test: ,build(deps): \""
     )
-    return eval(content_str)
+
 
 files_paths_to_update = [
     ".github/workflows/pull-request-tasks.yml",
     ".github/workflows/pull-request-checks.yml",
 ]
-print(f"CWD: {os.getcwd()}")
 for file_path in files_paths_to_update:
     if update_pr_title_checker(file_path):
         break
